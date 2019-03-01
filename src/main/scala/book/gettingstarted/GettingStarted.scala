9a026bfeebdba5504b6fe91b8f6a0eaa75a15c7c
Eric Tobin <eatobin@gmail.com>
2019-02-28 21:42:05 -0600

    parens gone

--- src/main/scala/book/gettingstarted/GettingStarted.scala
+++ src/main/scala/book/gettingstarted/GettingStarted.scala
@@ -63,7 +63,7 @@ object GettingStarted {
     println(formatResult("absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
     println(formatResult("increment", 7, (x: Int) => x + 1))
-    println(formatResult("increment2", 7, (x) => x + 1))
+    println(formatResult("increment2", 7, x => x + 1))
     println(formatResult("increment3", 7, x => x + 1))
     println(formatResult("increment4", 7, _ + 1))
     println(formatResult("increment5", 7, x => {
