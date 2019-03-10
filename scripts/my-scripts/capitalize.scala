81234f79d51c798e37f8bb4b967eb0faf5849a7e
Eric Tobin <eatobin@gmail.com>
2019-03-10 16:29:59 -0500

    capitalize

---
+++ scripts/my-scripts/capitalize.scala
@@ -0,0 +1,15 @@
+def capitalize(word: String): String = {
+  @annotation.tailrec
+  def go(word: String, accum: String): String = {
+    if (word.trim.isEmpty)
+      accum.reverse
+    else
+      go(word.tail, word.head.toUpper.toString ++ accum)
+  }
+
+  go(word, "")
+}
+
+capitalize("dogs")
+
+// scala> :load ./scripts/my-scripts/capitalize.scala
