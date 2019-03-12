d96021a9bb04c3d29a86ea991d456e9b8b985630
Eric Tobin <eatobin@gmail.com>
2019-03-12 18:02:44 -0500

    new master added

---
+++ fpinscala-master/answers/src/main/scala/fpinscala/streamingio/Partial.scala
@@ -0,0 +1,14 @@
+package fpinscala.streamingio
+
+import scala.language.higherKinds
+
+/*
+ * A context in which exceptions can be caught and
+ * thrown.
+ */
+trait Partial[F[_]] {
+  def attempt[A](a: F[A]): F[Either[Throwable, A]]
+
+  def fail[A](t: Throwable): F[A]
+}
+
