22465a74afaaff5c6395a2f1a025f43b4164df41
Eric Tobin <eatobin@gmail.com>
2019-03-12 15:51:52 -0500

    ex5

--- scripts/book/exercise5.scala
+++ scripts/book/exercise5.scala
@@ -1,5 +1,6 @@
 // Exercise 5: Implement `compose`
-def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
+def compose[A, B, C](f: B => C, g: A => B): A => C =
+  a => f(g(a))
 
 def composeTest(): Int = {
   val a: Int = 8
@@ -13,5 +14,12 @@ composeTest()
 println(compose((b: Int) => b * 1, (a: Int) => a * 1))
 println(composeTest())
 println(compose((b: Int) => b * 10, (a: Int) => a + 5)(3))
+
+def f(b: Int): Int = b / 2
+def g(a: Int): Int = a + 2
+
+assert(compose(f, g)(0) != compose(g, f)(0))
+assert(compose(f, g)(2) == 2)
+assert(compose(g, f)(2) == 3)
 
 // scala> :load ./scripts/book/exercise5.scala
