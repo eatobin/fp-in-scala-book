89feb88ba3225f95e520568b135a6e8b7ecf2f35
Eric Tobin <eatobin@gmail.com>
2019-03-12 15:05:57 -0500

    ex3

--- scripts/book/exercise3.scala
+++ scripts/book/exercise3.scala
@@ -1,9 +1,8 @@
 // Exercise 3: Implement `curry`.
 // Note that `=>` associates to the right, so we could
 // write the return type as `A => B => C`
-def curry[A, B, C](f: (A, B) => C): A => B => C = {
+def curry[A, B, C](f: (A, B) => C): A => B => C =
   a => b => f(a, b)
-}
 
 def curryTest(): Int = {
   val a: Int = 2
@@ -23,5 +22,11 @@ println(curry((a: Int, b: Int) => a - b))
 
 println(curry((a: Int, b: Int) => a - b)(22)(7)) // BEST
 println(curry((a: String, b: String) => a + b)("Here's word 1 and ")("here's word 2."))
+
+def f(a: Int, b: Int): Int = a + b
+def g(a: Int)(b: Int): Int = a + b
+
+assert(curry(f)(1)(1) == f(1, 1))
+assert(curry(f)(1)(1) == g(1)(1))
 
 // scala> :load ./scripts/book/exercise3.scala
